cdc47aeebee55f694fc6dd5c4f32ec1796b6972c
Brett Hoes <bretthoes@gmail.com>
2023-06-10 01:18:38 -0400

    build out drinks page

--- components/drinkitem.tsx
+++ components/drinkitem.tsx
@@ -1,5 +1,11 @@
+import React from 'react'
 import styles from '../styles/drinkitem.module.css'
 import 'material-icons/iconfont/material-icons.css'
+
+interface Option {
+  name: string
+  quantity: number
+}
 
 interface DrinkItemProps {
   name: string
@@ -7,30 +13,35 @@ interface DrinkItemProps {
   price: number
   category: string
   subcategory: string
-  options: string[]
+  options: Option[]
 }
 
-export default function MenuItem(
-  {
-    name,
-    description,
-    price,
-    subcategory,
-    options
-  }: DrinkItemProps) {
-  const renderSpicyIcons = () => {
-    const icons = [];
-  };
-
+export default function MenuItem({
+  name,
+  description,
+  price,
+  subcategory,
+  options,
+}: DrinkItemProps) {
   return (
     <div className={styles.item}>
       <div className={styles.inline}>
-        {price > 0 && (
-          <p className={styles.price}>{price.toFixed(2)}</p>
-        )}
+        {price > 0 && <p className={styles.price}>{price.toFixed(2)}</p>}
         <h2 className={styles.name}>{name}</h2>
       </div>
       <p className={styles.description}>{description}</p>
+      {options && (
+        <div className={styles.optionsContainer}>
+          <div className={styles.grid}>
+            {options.map((option, index) => (
+              <div key={index} className={styles.option}>
+                <p className={styles.optionName}>{option.name}</p>
+                <p className={styles.optionQuantity}>{option.quantity}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- pages/drinks.tsx
+++ pages/drinks.tsx
@@ -1,16 +1,21 @@
 import drinksData from '../public/drinks.json'
 import menuSectionsData from '../public/menusections.json'
-import styles from '../styles/menu.module.css'
+import styles from '../styles/drinks.module.css'
 import SectionTitle from '../components/sectiontitle'
 import DrinkItem from '../components/drinkitem'
 
-interface DrinkItem {
+interface Option {
+  name: string
+  quantity: number
+}
+
+interface Drink {
   name: string
   description: string
   price: number
   category: string
   subcategory: string
-  options: string[]
+  options: Option[]
 }
 
 interface MenuSection {
@@ -21,42 +26,41 @@ interface MenuSection {
 }
 
 interface PageProps {
-  drinks: DrinkItem[],
+  drinks: Drink[]
   MenuSections: MenuSection[]
 }
 
 export default function Drinks({ drinks, MenuSections }: PageProps) {
-  const drinkItems: DrinkItem[] = drinks || drinksData
-  const sections: MenuSection[] = MenuSections || menuSectionsData;
-  const MENU_CATEGORY_TYPE = "drinks";
+  const drinkItems: Drink[] = drinks || drinksData
+  const sections: MenuSection[] = MenuSections || menuSectionsData
+  const MENU_CATEGORY_TYPE = 'drinks'
 
   // Sort menu sections by order
   sections.sort((a, b) => a.order - b.order);
 
-  // TODO add navbar links at top of menu for navigating to sections quickly
   return (
     <>
-      <div className={styles.menu}>
+      <div className={styles.container}>
         {sections
-        .filter((menuSection) => menuSection.category === MENU_CATEGORY_TYPE)
-        .map((section) => (
-          <div key={section.name}>
-            <SectionTitle title={section.name} description={section.description} />
-            <div className={styles.menu}>
-              {drinkItems
-                .filter((drinkItem) => drinkItem.category === section.name)
-                .map((drinkItem, index) => (
-                  <DrinkItem
-                    key={index}
-                    name={drinkItem.name}
-                    description={drinkItem.description}
-                    price={drinkItem.price}
-                    category={drinkItem.category}
-                    subcategory={drinkItem.subcategory}
-                    options={drinkItem.options}
-                  />
-                ))}
-            </div>
+          .filter((menuSection) => menuSection.category === MENU_CATEGORY_TYPE)
+          .map((section) => (
+            <div key={section.name}>
+              <SectionTitle title={section.name} description={section.description} />
+              <div className={styles.drinks}>
+                {drinkItems
+                  .filter((drinkItem) => drinkItem.category === section.name)
+                  .map((drinkItem, index) => (
+                    <DrinkItem
+                      key={index}
+                      name={drinkItem.name}
+                      description={drinkItem.description}
+                      price={drinkItem.price}
+                      category={drinkItem.category}
+                      subcategory={drinkItem.subcategory}
+                      options={drinkItem.options}
+                    />
+                  ))}
+              </div>
           </div>
         ))}
       </div>
